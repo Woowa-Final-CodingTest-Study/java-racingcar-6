b54ca63941a16f9b813803fb37ec20f0432013f1
dmswl6310 <dmswl6310@gmail.com>
2023-11-24 09:09:06 +0900

    feat(Racing): 0~9 사이 랜덤 값 구하기

--- src/main/java/racingcar/domain/Racing.java
+++ src/main/java/racingcar/domain/Racing.java
@@ -1,5 +1,7 @@
 package racingcar.domain;
 
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,5 +14,18 @@ public class Racing {
         for (String name : carName) {
             cars.add(new Car(name));
         }
+    }
+
+    public void execute() {
+        for (Car car : cars) {
+        }
+    }
+
+    public int getRandomNumber() {
+        return pickNumberInRange(0, 9);
+    }
+
+    public void getResults() {
+
     }
 }
