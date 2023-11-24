f619c8d16bd5315e117f755129c994b0219d4524
dmswl6310 <dmswl6310@gmail.com>
2023-11-24 09:25:34 +0900

    feat(OutputView): 공동 우승자가 있을 시 ','로 구분하여 출력

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -21,4 +21,13 @@ public class OutputView {
         }
         System.out.print("\n");
     }
+
+    public static void printFinalWinner(List<String> winners) {
+        System.out.print("최종 우승자 : ");
+        
+        System.out.print(winners.get(0));
+        for (int i = 1; i < winners.size(); i++) {
+            System.out.print(", " + winners.get(i));
+        }
+    }
 }
