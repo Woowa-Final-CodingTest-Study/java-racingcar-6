e142ce60121b49a858f6f1c9b0a0f0a7943974bc
dmswl6310 <dmswl6310@gmail.com>
2023-11-24 09:19:36 +0900

    feat(OutputView): 시행횟수만큼 결과 출력

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,5 +1,7 @@
 package racingcar.view;
 
+import java.util.List;
+
 public class OutputView {
     public static void printRequestCarName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -11,5 +13,12 @@ public class OutputView {
 
     public static void printResultTitle() {
         System.out.println("\n실행 결과");
+    }
+
+    public static void printResult(List<String> results) {
+        for (String result : results) {
+            System.out.println(result);
+        }
+        System.out.print("\n");
     }
 }
