0d5fde9e4ff63f93203f3d055b3658a0d74e8fa9
Chenowoo <cjsgusdn14@naver.com>
2023-11-26 02:40:30 +0900

    feat(MainController): 시도 횟수를 받는 기능 구현

--- src/main/java/racingcar/controller/MainController.java
+++ src/main/java/racingcar/controller/MainController.java
@@ -3,6 +3,7 @@ package racingcar.controller;
 import racingcar.domain.Player;
 import racingcar.domain.ScoreCalculator;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 import java.util.List;
 
@@ -10,11 +11,23 @@ public class MainController {
 
     ScoreCalculator scoreCalculator = new ScoreCalculator();
     InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
 
-    public void inputPlayerNames() {
+    public void run() {
+        List<Player> players = inputPlayerNames();
+        int attemptCount = inputAttemptNumber();
+    }
+
+    public List<Player> inputPlayerNames() {
+        outputView.printInputPlayerName();
         String input = inputView.getUserInput();
         List<Player> players = scoreCalculator.initializePlayerScore(input);
+        return players;
+    }
 
+    public int inputAttemptNumber() {
+        outputView.printInputAttemptCount();
+        return Integer.parseInt(inputView.getUserInput());
     }
 
 
