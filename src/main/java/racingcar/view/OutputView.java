d87ac960d72f964dac00b74c499840dad19203b1
Chenowoo <cjsgusdn14@naver.com>
2023-11-26 02:37:15 +0900

    feat(OutputView): 사용자로부터 입력받는 출력 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,17 @@
 package racingcar.view;
 
+import racingcar.domain.Player;
+
+import java.util.List;
+
 public class OutputView {
+    public void printInputPlayerName() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public void printInputAttemptCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+
 }
