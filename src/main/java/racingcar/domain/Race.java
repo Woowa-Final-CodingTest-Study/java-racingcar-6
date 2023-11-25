08e51c5f94f6e443ee697cb278441017603fa857
Chenowoo <cjsgusdn14@naver.com>
2023-11-26 02:38:21 +0900

    feat(Race): 자동차가 전진 또는 스탑인지 결정하는 기능 구현

--- src/main/java/racingcar/domain/Race.java
+++ src/main/java/racingcar/domain/Race.java
@@ -1,4 +1,14 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Race {
+
+    public boolean decideMovement() {
+        int randomNumber = Randoms.pickNumberInRange(0, 9);
+        if (randomNumber >= 4) {
+            return true;
+        }
+        return false;
+    }
 }
