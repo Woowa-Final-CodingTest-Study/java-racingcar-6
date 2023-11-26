913fe071b6af1036766a9ea12b56a378ed533c7e
Jonghyeok Lee <endb1228@gmail.com>
2023-11-26 12:35:34 +0900

    Test: car의 전진 여부에 대한 테스트 추가
    - car의 전진 여부에 대한 테스트 추가

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -1,13 +1,29 @@
 package racingcar.model;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarTest {
 
-    @Test
-    void goForward() {
-        //TODO
+    @ParameterizedTest
+    @ValueSource(ints = {4,5,6,7,8,9})
+    void go_forward_with_4_or_greater(int value) {
+        String name = "car";
+        Car car = new Car(name);
+        car.goForward(value);
+        Assertions.assertThat(car.roundResult()).isEqualTo(name + " : " + "-");
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0,1,2,3})
+    void stop_with_3_or_less(int value) {
+        String name = "car";
+        Car car = new Car(name);
+        car.goForward(value);
+        Assertions.assertThat(car.roundResult()).isEqualTo(name + " : ");
     }
 }
