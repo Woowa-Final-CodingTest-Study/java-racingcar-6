0f417eeb1df319bc8438fc0d0452d3fd2313352e
Chenowoo <cjsgusdn14@naver.com>
2023-11-26 18:07:27 +0900

    test(racingcar): 테스트 코드 구현

--- src/test/java/racingcar/domain/ScoreCalculatorTest.java
+++ src/test/java/racingcar/domain/ScoreCalculatorTest.java
@@ -1,14 +1,12 @@
 package racingcar.domain;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class ScoreCalculatorTest {
 
